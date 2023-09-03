bfd612f54661cf8552ebc426d6fe032fb486823b
George Sagiannis <gsagiannis@gmail.com>
2023-09-03 13:29:47 +0300

    added in component

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -2,7 +2,17 @@ const ListContacts = ({ contacts }) => {
   return (
     <ol className="contact-list">
       {contacts.map((contact) => (
-        <li key={contact.id}>{contact.name}</li>
+        <li key={contact.id}>
+          <div
+            className="contact-avatar"
+            style={{ backgroundImage: `url(${contact.avatarURL})` }}
+          ></div>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button className="contact-remove">Remove</button>
+        </li>
       ))}
     </ol>
   );
