1f447d3e8941af051f14076f1541bcfb087ffce0
George Sagiannis <gsagiannis@gmail.com>
2023-09-12 15:30:18 +0300

    Turn create contact link into a Link component.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const ListContacts = ({ contacts, onDeleteContact, onNavigate }) => {
+const ListContacts = ({ contacts, onDeleteContact }) => {
   const [query, setQuery] = useState("");
 
   const updateQuery = (query) => {
@@ -29,9 +30,9 @@ const ListContacts = ({ contacts, onDeleteContact, onNavigate }) => {
           value={query}
           onChange={(event) => updateQuery(event.target.value)}
         />
-        <a href="#create" onClick={onNavigate} className="add-contact">
+        <Link to="/create" className="add-contact">
           Add Contact
-        </a>
+        </Link>
       </div>
 
       {showingContacts.length !== contacts.length && (
@@ -73,5 +74,4 @@ ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
   onDeleteContact: PropTypes.func.isRequired,
 };
-
 export default ListContacts;
